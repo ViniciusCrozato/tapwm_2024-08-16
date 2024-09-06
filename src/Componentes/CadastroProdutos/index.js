e654ab45b9d076bbe772c116f70910fb9b563586
ViniciusCrozato <160658562+ViniciusCrozato@users.noreply.github.com>
2024-09-06 19:19:38 +0000

    Exclusão de dados da API - Concluída

--- src/Componentes/CadastroProdutos/index.js
+++ src/Componentes/CadastroProdutos/index.js
@@ -6,10 +6,16 @@ export default function CadastroProduto({carregaProdutos}) {
                 <h3 className="titulo"> Cadastro de Produtos</h3>
                 <form>
                     <label>Nome:</label>
-                    <input type="text" id="nome" name="nome"></input>
+                    <input type="text" id="nome" name="nome"></input> <br/>
                     <label>Preço:</label>
-                    <input type="text" id="preco" name="preco"></input>
-
+                    <input type="text" id="preco" name="preco"></input> <br/>
+                    <label>Foto</label>
+                    <input type="text" id="foto" name="foto"></input><br/>
+                    <label>Descrição</label>
+                    <input type="text" id="descricao" name="descricao"></input><br/>
+                    <label>Categoria</label>
+                    <input type="text" id="categoria" name="categoria"></input><br/>
+                    
                     <button type="button" onClick={adicionaProduto}>Adiciona</button>
                 </form>
             </div>
--- src/Componentes/Produto/index.js
+++ src/Componentes/Produto/index.js
@@ -1,15 +1,26 @@
+import axios from 'axios';
 import '../../index.css';
+import {MdDelete} from 'react-icons/md';
 
-const Produto = ({valor}) => {
+const Produto = ({valor, carregaProdutos}) => {
     return (
         <div className='cardProd'>
             <center>
                 <h3 className='titulo'>{valor.nome}</h3>
                 <p className='subtitulo'>Preço: {valor.preco}</p>
                 <img src={valor.foto} alt='Imagem do Produto' className='imagem'></img>
+                <p className='subtitulo'>Descrição: {valor.descricao}</p>
+                <p className='subtitulo'>Categoria: {valor.dcategoria}</p>
+
+                <MdDelete className='icone' onClick={()=>remover(valor.id)}/>
             </center>
         </div>
     )
+    function remover(id){
+        let api = "https://app-api-tapwm.onrender.com/api/produtos";
+        axios.delete(`${api}/${id}`).then(()=>carregaProdutos());
+    }
 }
+
 
 export default Produto;
